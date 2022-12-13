c31b15ca33d4b54d862ea070ec3a6a4fc3c60535
trangnguyen2809 <trangnguyennguyen2809@gmail.com>
2022-12-13 09:54:38 +0700

    update comment

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,17 +1,26 @@
 /* Global Variables */
 
-import {getTravel} from './getTravel'
+import { getTravel } from './getTravel'
 
 const formE = document.getElementById("form");
 const zipE = document.getElementById("zip");
+const timeE = document.getElementById("time");
 const btnGenerateE = document.getElementById("generate");
 
 const dateE = document.getElementById("date");
 const tempE = document.getElementById("temp");
 const contentE = document.getElementById("content");
 
-const click = ()=>{
-    getTravel(zipE.value, (data)=>{
+const click = () => {
+    const dateDes = Date.parse(timeE.value);
+    const now = new Date().getTime();
+    const remain = Math.floor((dateDes - now) / (24 * 60 * 60 * 1000));
+    if(remain <= 0){
+        alert("Time invalid");
+        return;
+    }
+    getTravel(zipE.value, (data) => {
+        dateE.innerHTML = `Remaining: ${remain} day`
         tempE.innerHTML = `Temp: ${data.temp} Celsius`;
         contentE.innerHTML = `<img src="${data.image}"/>`;
     })
